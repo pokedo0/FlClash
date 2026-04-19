c3f63b84dc63303894e4c023b929fc89814f6a6d
meister <qq@qq.com>
2026-04-19 12:31:10 +0800

    custom package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.follow.clash"
+        applicationId = "com.news.toutiao"
         minSdk = flutter.minSdkVersion
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = flutter.versionCode
